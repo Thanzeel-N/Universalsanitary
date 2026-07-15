f2a8e3b9432cd79a97a4a0dd5d69526572ac9281
Thanzeel-N <thanzeelmtn@gmail.com>
2026-07-15 15:18:41 +0530

    fix: update domain from .com to .in in sitemap and robots

--- universal_frontend/src/app/sitemap.ts
+++ universal_frontend/src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://universalsanitary.com'
+  const baseUrl = 'https://universalsanitary.in'
   
   return [
     {
